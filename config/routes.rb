82f4864d0dc1c888878c720fc8042bb85fa9dc9d
Daniel Philpot <Dphil300@gmail.com>
2017-08-05 18:08:07 -0400

    updated with comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  
 
-  resources :posts
   resources :users
-  resources :sessions
+
+  resources :posts do
+    resources :users
+    resources :sessions
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "posts#index"
-  
+
 end
---
+++ db/migrate/20170805181322_create_comments.rb
@@ -0,0 +1,16 @@
+class CreateComments < ActiveRecord::Migration[5.1]
+  def change
+    create_table :comments do |t|
+      t.text :content
+      t.references :post, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
+
+# t.references :post, foreign_key: true
+# t.references :user, foreign_key: true
+
+# t.timestamps
