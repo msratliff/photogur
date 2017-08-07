2362bc97e2689122c5c1d55586ac9ad7f43c7a80
Mark Ratliff <mark.s.ratliff@gmail.com>
2017-08-07 18:40:20 -0400

    Tags working

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
 	belongs_to :user
 	has_many :comments, dependent: :destroy
 
-	has_many :taggings
+	has_many :taggings 
 	has_many :tags, through: :taggings
 
 	def self.tagged_with(name)
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
 	get 'login', to: 'sessions#new', as: 'login'
 	get 'logout', to: 'sessions#destroy', as: 'logout'
   
+	get 'tags/:tag', to: 'posts#index', as: :tag
+
+
   resources :posts do
     resources :comments
-    resources :tags
   end
 
 	resources :users
