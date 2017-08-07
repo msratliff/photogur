c03e56dff47e04ae38d72b185c50c65ff8af0193
Mark Ratliff <mark.s.ratliff@gmail.com>
2017-08-07 09:48:55 -0400

    Seeded DB with Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+post_list = [
+  ["Panda", "Panda Panda Panda", "http://animals.sandiegozoo.org/sites/default/files/2016-09/animals_hero_panda.jpg", 3],
+  ["Snake", "Snake Snake Snake", "https://i.ytimg.com/vi/n174VWdSlCE/maxresdefault.jpg", 1],
+  ["Duck", "Duck Duck Duck", "https://greens.org.au/sites/greens.org.au/files/field/image/animals-duck-header.jpg", 3],
+  ["Snake", "Snake Snake Snake", "https://i.ytimg.com/vi/n174VWdSlCE/maxresdefault.jpg", 1],
+  ["Red Panda", "Red Panda Red Panda Red Panda", "https://ichef.bbci.co.uk/childrens-responsive-ichef-live/r/400/1x/cbbc/Animaltastic_Ambigous_Animals_Names_1024_576.jpg", 3],
+  ["Orangutan", "Orangutan Orangutan Orangutan", "http://i.huffpost.com/gen/1755634/images/o-ANIMALS-PONDERING-facebook.jpg", 1],
+  ["Zebra", "Zebra Zebra Zebra", "http://photos.mrkzy.com/wp-content/uploads/photos-mrkzy-animal-19183.jpg", 3]
+
+  
+]
+
+post_list.each do |name, description, url, user_id|
+  Post.find_or_create_by( name: name, description: description, url: url, user_id: user_id)
+end
