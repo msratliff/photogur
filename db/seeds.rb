2f1f471a642e83dc3ce5f450aa97bc2d33d30535
Daniel Philpot <Dphil300@gmail.com>
2017-08-11 08:41:11 -0400

    snowflake project F&F

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,36 @@
 i=0
 
 while i < 50 do
-	Post.find_or_create_by( name: "Bill Murray #{i}", description: "Knausgaard chia pok pok cardigan hammock. Skateboard church-key shoreditch williamsburg, pork belly iceland vexillologist man bun chia kale chips cronut direct trade meh biodiesel four loko. Before they sold out squid mumblecore, tilde raclette pitchfork man braid.", url: "https://www.fillmurray.com/#{rand(25..85)*10}/#{rand(25..85)*10}", user_id: rand(3))
-	
-
+	Post.find_or_create_by( name: "Cat - #{i}", description: "Knausgaard chia pok pok cardigan hammock. Skateboard church-key shoreditch williamsburg, pork belly iceland vexillologist man bun chia kale chips cronut direct trade meh biodiesel four loko. Before they sold out squid mumblecore, tilde raclette pitchfork man braid.", url: "https://www.placekitten.com/#{rand(25..85)*10}/#{rand(25..85)*10}", user_id: 2)
   	n=0
-
 	while n < 5 do
-		Comment.find_or_create_by(content: "Trust fund quinoa lyft everyday carry man bun slow-carb lumbersexual fam squid. Cray hammock cred put a bird on it.", post_id: i, user_id: rand(3))
+		Comment.find_or_create_by(content: "Trust fund quinoa lyft everyday carry man bun slow-carb lumbersexual fam squid. Cray hammock cred put a bird on it.", post_id: i, user_id: rand(4))
 		n+=1
 	end
-
 	i+= 1
+end
 
+
+i=0
+
+while i < 50 do
+	Post.find_or_create_by( name: "Bear - #{i}", description: "Knausgaard chia pok pok cardigan hammock. Skateboard church-key shoreditch williamsburg, pork belly iceland vexillologist man bun chia kale chips cronut direct trade meh biodiesel four loko. Before they sold out squid mumblecore, tilde raclette pitchfork man braid.", url: "https://www.placebear.com/#{rand(25..85)*10}/#{rand(25..85)*10}", user_id: 3)
+  	n=0
+	while n < 5 do
+		Comment.find_or_create_by(content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor vel arcu vitae varius. Donec eu ornare quam, nec ultricies magna. In dignissim ligula leo, at pellentesque massa efficitur at.", post_id: i, user_id: rand(4))
+		n+=1
+	end
+	i+= 1
+end
+
+i=0
+
+while i < 50 do
+	Post.find_or_create_by( name: "Bacon - #{i}", description: "Knausgaard chia pok pok cardigan hammock. Skateboard church-key shoreditch williamsburg, pork belly iceland vexillologist man bun chia kale chips cronut direct trade meh biodiesel four loko. Before they sold out squid mumblecore, tilde raclette pitchfork man braid.", url: "https://www.baconmockup.com/#{rand(25..85)*10}/#{rand(25..85)*10}", user_id: 4)
+  	n=0
+	while n < 5 do
+		Comment.find_or_create_by(content: "Donec tincidunt tincidunt egestas. Donec sit amet luctus nulla. Phasellus id erat quis enim gravida molestie. Phasellus semper mi ex, non congue felis consequat vel. Integer volutpat metus id commodo gravida.", post_id: i, user_id: rand(4))
+		n+=1
+	end
+	i+= 1
 end
